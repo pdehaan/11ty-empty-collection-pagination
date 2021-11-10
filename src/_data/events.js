1ec1b669435f4c3d2b936ac50e8b26b9283d9a88
Peter deHaan <peter@deseloper.com>
2021-11-09 19:16:42 -0800

    Return semi-random event results (based on date)

--- src/_data/events.js
+++ src/_data/events.js
@@ -1,7 +1,10 @@
 module.exports = () => {
-  return [];
-  // return [
-  //   {title: "Event 1", date: "2022/04/19"},
-  //   {title: "Event 2", date: "2021/09/30"}
-  // ];
+  // return [];
+
+  // Only return events from The Future(tm).
+  // This may or may not return 0 or more events.
+  return [
+    {title: "Event 1", date: "2022/04/19"},
+    {title: "Event 2", date: "2021/09/30"}
+  ].filter(event => Date.parse(event.date) >= Date.now());
 };
