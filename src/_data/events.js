6173c61791c1823cbb08e4245b03094c57fe27a9
Peter deHaan <peter@deseloper.com>
2021-11-09 20:02:47 -0800

    Option 2

--- src/_data/events.js
+++ src/_data/events.js
@@ -4,7 +4,9 @@ module.exports = () => {
   // Only return events from The Future(tm).
   // This may or may not return 0 or more events.
   return [
-    {title: "Event 1", date: "2022/04/19"},
-    {title: "Event 2", date: "2021/09/30"}
-  ].filter(event => Date.parse(event.date) >= Date.now());
+    { title: "Kernel Panic! At The Disco", date: "2022/04/19" },
+    { title: "LED Zeppelin", date: "2022/09/30" },
+  ]
+    .filter((event) => Date.parse(event.date) >= Date.now())
+    .sort((a, b) => a.date.localeCompare(b.date));
 };
